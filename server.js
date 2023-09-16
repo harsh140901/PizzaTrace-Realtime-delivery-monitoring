fc447e8948e4abe933fe0cc178d1c27396835ac3
harsh140901 <harshalkotkar2001@gmail.com>
2023-09-17 03:05:56 +0530

    Add a cart page.login and register page

--- server.js
+++ server.js
@@ -4,19 +4,33 @@ const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 // Port for server including live server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-  res.render('home');
-});
 
 // Assets
 app.use(express.static('Public'));
+
 // set template engine 
 app.use(expressLayout);
 app.set('views' ,path.join(__dirname,'/resources/views'));
 app.set('view engine' , 'ejs');
 
+// routes
+app.get('/', (req,res) => {
+  res.render('home');
+});
+
+app.get('/cart', (req,res) => {
+  res.render('customers/cart');
+});
+
+app.get('/login', (req,res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req,res) => {
+  res.render('auth/register');
+});
 // making server to listen request from client
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
