ac88ae255bd5c796960a8aedfe8eca1e7df24fb4
harsh140901 <harshalkotkar2001@gmail.com>
2023-09-18 02:07:24 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,23 @@
+import axios from 'axios';
+import Noty from "noty";
 
-console.log("Hello from server");
+let addToCart = document.querySelectorAll('.add-to-cart');
+let cartCounter = document.querySelector('#cartCounter');
+function updateCart(pizza){
+
+  axios.post('/update-cart',pizza).then(res => {
+    cartCounter.innerText = res.data.totalQty;
+    new Noty({
+      type: 'success',
+      timeout : 1000,
+      text: "Item is added to cart."
+    }).show();
+  })
+}
+addToCart.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+      let pizza = JSON.parse(btn.dataset.pizza);
+      updateCart(pizza);
+  });
+});
+i
--- server.js
+++ server.js
@@ -1,36 +1,64 @@
+require('dotenv').config();
 const express = require('express'); 
 const app = express();
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo')(session);
 // Port for server including live server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
+// Database connection
+const url = 'mongodb://127.0.0.1/pizza';
+mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+mongoose.connection
+    .once('open', function () {
+      console.log('Database connected...');
+    })
+    .on('error', function (err) {
+      console.log(err);
+    });
+
+
+//session store
+let mongoStore = new MongoDbStore({
+  mongooseConnection : connection,
+  collection : 'session'
+})
+// session config
+
+app.use(session({
+  secret : process.env.COOKIE_SECRET,
+  resave : false,
+  store : mongoStore,
+  saveUninitialized : false,
+  cookie : {maxAge : 1000 * 60 *60 *24} // 24 hrs
+}));
+
+app.use(flash());
 // Assets
 app.use(express.static('Public'));
+app.use(express.json());
 
+// global middleware
+app.use((req,res,next) => {
+  res.locals.session = req.session;
+  next();
+});
 // set template engine 
 app.use(expressLayout);
 app.set('views' ,path.join(__dirname,'/resources/views'));
 app.set('view engine' , 'ejs');
 
 // routes
-app.get('/', (req,res) => {
-  res.render('home');
-});
+require('./routes/web')(app);
 
-app.get('/cart', (req,res) => {
-  res.render('customers/cart');
-});
 
-app.get('/login', (req,res) => {
-  res.render('auth/login');
-});
-
-app.get('/register', (req,res) => {
-  res.render('auth/register');
-});
 // making server to listen request from client
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
